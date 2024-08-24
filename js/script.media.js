0db776a0844d943228d9b7e1d5ce7ca5c70acc95
DjoAHP <djodjo.vely@gmail.com>
2024-08-24 23:26:20 +0200

    changement

--- js/script.media.js
+++ js/script.media.js
@@ -16,9 +16,45 @@ const sr = ScrollReveal ({
     reset: true
 });
 
-
-sr.reveal('.icons',{delay:500, origin: 'left'});
 sr.reveal('.scroll-down',{delay:500, origin: 'top'});
+
+// SECTION 1 
+const tiltedSection = document.querySelector(".tilted-section")
+const tiltedCard = document.querySelector(".tilted-section__card")
+
+if (window.matchMedia("(pointer:fine)").matches) {
+
+      tiltedSection.addEventListener("mousemove", handleTiltEffect)
+
+      function handleTiltEffect(e) {
+            const tiltedSectionDimensions = tiltedSection.getBoundingClientRect()
+
+            const mouseXInSection = e.clientX - tiltedSectionDimensions.left,
+                  mouseYInSection = e.clientY - tiltedSectionDimensions.top
+
+            const elementMiddleX = tiltedSectionDimensions.width / 2,
+                  elementMiddleY = tiltedSectionDimensions.height / 2;
+
+            const maxTiltX = 30,
+                  maxTiltY = 30;
+
+
+            const tiltAngleY = ((mouseXInSection - elementMiddleX) / elementMiddleX) * maxTiltX
+            const tiltAngleX = ((mouseYInSection - elementMiddleY) / elementMiddleY) * maxTiltY
+
+            tiltedCard.style.transform = `rotateY(${tiltAngleY}deg) rotateX(${-tiltAngleX}deg)`
+            console.log(elementMiddleX, elementMiddleY)
+      }
+
+
+      tiltedSection.addEventListener("mouseout", resetTiltOnMouseOut)
+
+      function resetTiltOnMouseOut() {
+            tiltedCard.style.transform = `rotateY(${0}deg) rotateX(${0}deg)`
+      }
+}
+// SECTION 1 
+
 
 
 
